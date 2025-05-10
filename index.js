1de538820af3f36b9ba628b42d8b9a54f90d5ba4
AmkumarA <amit123.ap62@gmail.com>
2025-05-10 17:54:45 +0530

    add session

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const youtubeRoutes = require('./routes/youtube.routes');
 const facebookRoutes = require('./routes/facebook.routes');
-const twitterRoutes = require('./routes/twitter.routes')
+const twitterRoutes = require('./routes/twitter.routes');
+const session = require('express-session');
 const app = express();
 const port = 3000;
 // Step 1: Configure OAuth2
@@ -10,6 +11,12 @@ const port = 3000;
 
 const path = require('path');
 app.use(express.static('public'));
+// twitter session
+app.use(session({
+    secret: 'twitter_secret',
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
